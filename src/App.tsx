32d1582e565abfe3e996aee305f9dc28388be6ea
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-01-20 11:14:40 +0300

    get api-query & state-of-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import FavouritesPages from './pages/FavouritesPages';
+import HomePage from './pages/HomePage';
+import Navigation from './components/Navigation';
 
 
 function App() {
   return (
-    <h1>hello</h1>
+    <>
+    <Navigation />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/favourites' element={ <FavouritesPages /> }/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/FavouritesPages.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FavouritesPages() {
+  return (
+    <div>FavouritesPages</div>
+  )
+}
